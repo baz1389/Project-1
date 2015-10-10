a8678bc004d04c59e07f47550eeb2294133acce2
Ben <benjamin.a.spencer@gmail.com>
2015-10-10 13:06:31 -0400

    add jQ click function

---
+++ code.js
@@ -0,0 +1,24 @@
+'use strict';
+
+//Javascript Logic
+
+
+
+
+
+
+
+
+
+//jQuery Logic
+$(document).ready(function() {
+  //
+  $('.box').on('click', function(event) {
+    console.log(event);
+    alert('I was clicked'); //maybe an if else statement here
+  });
+
+//add more functions here for jQuery
+});
+
+
