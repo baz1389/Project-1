3b268fe75f1948c776643b17bb1482db4946aac2
Ben <benjamin.a.spencer@gmail.com>
2015-10-10 15:51:12 -0400

    added board array and comments to various portions that need to be done

--- code.js
+++ code.js
@@ -3,6 +3,24 @@
 //Javascript Logic
 
 
+//cell-0-0 to -0-2 are the left column from top to bottom
+//cell-1-0 to 1-2 are the center column from top to bottom
+//cell-2-0 to 2-2 are the right column from top to bottom
+var gameBoard = ['cell-0-0', 'cell-1-0', 'cell-2-0',
+                'cell-0-1', 'cell-1-1', 'cell-2-1',
+                'cell-0-2', 'cell-1-2', 'cell-2-2']
+
+
+//check for victory
+
+
+//reset board
+
+//the game was a draw
+
+//shows the winner and resets the board using reset function
+
+
 
 
 
@@ -12,7 +30,7 @@
 
 //jQuery Logic
 $(document).ready(function() {
-  //
+  //detects a click on one of the nine boxes
   $('.box').on('click', function(event) {
     console.log(event);
     alert('I was clicked'); //maybe an if else statement here
