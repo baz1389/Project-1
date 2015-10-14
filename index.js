250899c669eab886001510d4a42fa0ce6878fa53
Ben <benjamin.a.spencer@gmail.com>
2015-10-14 18:47:16 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,90 @@
+'use strict';
+var tttapi = {
+  gameWatcher: null,
+  ttt: 'http://ttt.wdibos.com',
+
+  ajax: function(config, cb) {
+    $.ajax(config).done(function(data, textStatus, jqxhr) {
+      cb(null, data);
+    }).fail(function(jqxhr, status, error) {
+      cb({jqxher: jqxhr, status: status, error: error});
+    });
+  },
+
+  register: function register(credentials, callback) {
+    this.ajax({
+      method: 'POST',
+      url: this.ttt + '/users',
+      contentType: 'application/json; charset=utf-8',
+      data: JSON.stringify(credentials),
+      dataType: 'json'
+    }, callback);
+  },
+
+  login: function login(credentials, callback) {
+    this.ajax({
+      method: 'POST',
+      url: this.ttt + '/login',
+      contentType: 'application/json; charset=utf-8',
+      data: JSON.stringify(credentials),
+      dataType: 'json'
+    }, callback);
+  },
+
+  //Authenticated api actions
+  listGames: function (token, callback) {
+    this.ajax({
+      method: 'GET',
+      url: this.ttt + '/games',
+      headers: {
+        Authorization: 'Token token=' + token
+      },
+      dataType: 'json'
+    }, callback);
+  },
+
+  createGame: function (token, callback) {
+    this.ajax({
+      method: 'POST',
+      url: this.ttt + '/games/',
+      headers: {
+        Authorization: 'Token token=' + token
+      },
+      dataType: 'json',
+      data: JSON.stringify({}),
+      contentType: 'application/json; charset=utf-8'
+    }, callback);
+  },
+
+  showGame: function (id, token, callback) {
+    this.ajax({
+    }, callback);
+  },
+
+  joinGame: function (id, token, callback) {
+    this.ajax({
+    }, callback);
+  },
+
+  markCell: function (id, data, token, callback) {
+    this.ajax({
+      method: 'PATCH',
+      url: this.ttt + '/games/' + id,
+      headers: {
+        Authorization: 'Token token=' + token
+      },
+      contentType: 'application/json; charset=utf-8',
+      data: JSON.stringify(data),
+      dataType: 'json'
+    }, callback);
+  },
+
+  watchGame: function (id, token) {
+    var url = this.ttt + '/games/' + id + '/watch';
+    var auth = {
+      Authorization: 'Token token=' + token
+    };
+    this.gameWatcher = resourceWatcher(url, auth); //jshint ignore: line
+    return this.gameWatcher;
+  }
+};
